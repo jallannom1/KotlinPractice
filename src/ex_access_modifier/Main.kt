97796ef39ee32b5cd862ddef9d3ac250c910e645
ssoonnii <jallannom2@gmail.com>
2020-05-19 11:24:42 +0900

    Access Modifier EX

---
+++ src/ex_access_modifier/Main.kt
@@ -0,0 +1,14 @@
+package ex_access_modifier
+
+class Rectangle(private val width: Int,
+                private val height: Int)
+{
+    val area: Int
+        get() = width * height
+}
+
+fun main(args:Array<String>){
+    val rect = Rectangle(5, 7)
+    // println(rect.width) error
+    println(rect.area)
+}
