13b38277676724d42440d219d52c7ee8f04eb121
ssoonnii <jallannom2@gmail.com>
2020-05-21 11:02:07 +0900

    Generic to Class and Interface

---
+++ src/ex_generic_to_class_and_interface/Main.kt
@@ -0,0 +1,15 @@
+package ex_generic_to_class_and_interface
+
+fun main(args: Array<String>){
+    val pair: Pair<Int, Double> = Pair<Int, Double>(15, 9.12)
+    println(pair.toString())
+}
+/*
+// 위 문법 추천해 주어서 바꿈
+
+fun main(args: Array<String>){
+    val pair: Pair<Int, Double>
+    pair = Pair<Int, Double>(15, 9.12)
+    println(pair.toString())
+}
+*/
