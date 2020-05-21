67f95841e7a28f2d9dc2e828e3951a4ed09ac92c
ssoonnii <jallannom2@gmail.com>
2020-05-21 16:14:48 +0900

    Enum Class Members Test

---
+++ src/ex_enum_class_members/Main.kt
@@ -0,0 +1,22 @@
+package ex_enum_class_members
+
+//
+
+enum class Mode(val number: Int){
+    SELECTION(0),
+    PEN(1),
+    SHAPE(2),
+    ERASER(3);
+
+    fun printNumber(){
+        println("모드: $number")
+    }
+}
+
+fun main(args: Array<String>){
+    // 현재 선택된 모드
+    val mode: Mode = Mode.ERASER
+
+    println(mode.number)
+    mode.printNumber()
+}
