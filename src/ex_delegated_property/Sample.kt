829d17e3c8e8b5448f402d65d9f8d72ef11f360f
ssoonnii <jallannom2@gmail.com>
2020-05-25 10:47:32 +0900

    Delegated Property Test Mod

--- src/ex_delegated_property/Sample.kt
+++ src/ex_delegated_property/Sample.kt
@@ -1,12 +1,9 @@
 package ex_delegated_property
 
-<<<<<<< HEAD
-=======
 /*
 프로퍼티의 Getter/Setter 구현을 다른 객체에 맡길 때 사용하는 문법
  */
 
->>>>>>> bdde933b8d071c4c426b70ac463ca58cfd73bfd4
 class Sample{
     var number: Int by OnlyPositive()
 }
--- src/ex_delegated_property/SquareDelegator.kt
+++ src/ex_delegated_property/SquareDelegator.kt
@@ -1,29 +1,17 @@
 package ex_delegated_property
 
-<<<<<<< HEAD
-=======
 // 프로퍼티 선언문 뒤에 by 객체를 적으면 해당 객체가 프로퍼티의 Getter/Setter를 대리하게 된다.
 
->>>>>>> bdde933b8d071c4c426b70ac463ca58cfd73bfd4
 import kotlin.reflect.KProperty
 
 class OnlyPositive{
     private var realValue: Int = 0
 
-<<<<<<< HEAD
     operator fun getValue(thisRef: Any?, property: KProperty<*>): Int{
         return realValue
     }
 
-    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: Int){
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: Int) {
         realValue = if (value > 0) value else 0
-=======
-    operator fun getValue(thisRef: Any?, perperty: KProperty<*>): Int{
-        return realValue
-    }
-
-    operator fun setValue(thisRef: Any?, perperty: KProperty<*>, value: Int){
-        realValue = if(value > 0) value else 0
->>>>>>> bdde933b8d071c4c426b70ac463ca58cfd73bfd4
     }
 }
